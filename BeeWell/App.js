0a98ce0234354e8f396d76383cd7879b2ea9c481
raveenajain <jain.rav@husky.neu.edu>
2019-11-01 17:31:54 -0400

    init expo for react native

---
+++ BeeWell/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Image } from 'react-native';
+import { Button } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Image source={require('./assets/tempback.jpg')}/>
+      <Button title="Hello Button" onPress={()} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
