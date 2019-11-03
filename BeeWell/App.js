cb604e2be4726fa78da37d710d9c8bc3b8ac130e
raveenajain <jain.rav@husky.neu.edu>
2019-11-03 10:27:25 -0500

    web host attempt

--- BeeWell/App.js
+++ BeeWell/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { Image } from 'react-native';
-import { Button } from 'react-native';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, Button, TouchableHighlight, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const handlePress = () => false;
   return (
     <View style={styles.container}>
-      <Image source={require('./assets/tempback.jpg')}/>
-      <Button title="Hello Button" onPress={()} />
+      <Image
+        style={styles.background}
+        source={require('./assets/fullBack.png')}
+      />
+      <TouchableHighlight style={styles.waterBottle} onPress={handlePress}>
+      <Image
+        source={require('./assets/icons/water.png')}
+      />
+      </TouchableHighlight>
     </View>
   );
 }
@@ -16,7 +22,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  background: {
     alignItems: 'center',
     justifyContent: 'center',
+    flex: 1,
+    zIndex: 2
   },
+  waterBottle: {
+    height: '10%',
+    width: '10%',
+    zIndex: 1
+  }
 });
